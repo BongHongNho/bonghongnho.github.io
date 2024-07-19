5eb6a3a3cf1a0b34a6277d052159020c4c2ab72b
LeaderPro <107822089+BongHongNho@users.noreply.github.com>
2024-07-20 00:05:25 +0700

    Create update.js

---
+++ update.js
@@ -0,0 +1,6 @@
+{   "dialogTitle": "New version available",
+    "dialogMessage": "New version is ready for update. Please update the app for a better experience!",
+    "latestBuild": "535009163",
+    "latestVersion": "4.135.0",
+    "updateLink": "https://an1.com/2449-dead-target-zombie-mod-apk.html"
+  }
